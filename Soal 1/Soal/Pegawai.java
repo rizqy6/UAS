6fc6ac4f08cff17aec6f0dc46b71a543f8aab121
rizqy6 <fazarizqy6@gmail.com>
2023-01-08 15:28:03 +0700

    tambahan komen

--- Soal 1/Soal/Pegawai.java
+++ Soal 1/Soal/Pegawai.java
@@ -2,39 +2,44 @@ package Soal;
 
 
 public class Pegawai{
+    //Instance variabel dengan akses modifier private ditandai dengan (-) di dalam UML
     private String nama;
     private String NIP;
 
-
+    //Sebuah Constructor dengan nilai NULL
+    public Pegawai(){
+        
+    }
+    
+    //Penerapan Overloading cunstructor karena memiliki paramter yang berbeda dengan constructor sebelumnya
     public  Pegawai(String nama, String NIP){
         setNama(nama);
         setNIP(NIP);
     }
 
-    public Pegawai(){
-        // this.nama = nama;
-        // this.NIP = NIP;
 
-        // System.out.println("Nama" + nama + " NIP" + NIP);
-    }
-
+    //Kode dibawah ini merupakan konsep Encapulation
+    // Method void paramete
     protected void setNama(String newNama){
         this.nama = newNama;
     }
 
+    //Pemanggilan instance nama dalam kasus akses modifier private
     public String getnewNama(){
         return nama;
     }
 
+    // Method void paramete
     protected void setNIP(String newNIP){
         this.NIP = newNIP;
     }
 
+    //Pemanggilan instance NIP dalam kasus akses modifier private
     public String getnewNIP(){
         return NIP;
     }
 
-
+    // ini hanya pengetesan saja
     public static void main(String[] args){
         Pegawai pegawai = new Pegawai("rix","22323");
         pegawai.setNama("Pegawai");
--- Soal2/SepedaMotor.java
+++ Soal2/SepedaMotor.java
@@ -1,21 +1,32 @@
 package Soal2;
 
+
+//Sebuah class abstarct ditandai dengan penulisan abstarct
 public abstract class SepedaMotor {
     
+    //Pembuatan instance variable String 
     String merk;
 
+
+    //Pembuatan instance variable integer bernilai awal 0
     int gerigi = 0;
 
+    //Terdapat interface dengan method abstarct
     interface OperasiGergigi{
         abstract void tambahGergigi();
         abstract void kurangGergigi();
     }
 
+    //static class  MotorSport yang diturunkan dari classSepedaMotor dan pengaplikasian implements OperasiGergigi
+
     static class MotorSport extends SepedaMotor implements OperasiGergigi{
+
+        //Constructor motorsport yang memiliki parameter String merk
         public MotorSport (String merk){
             
         }
-
+        // Dibawah ini terdapat override karena class MotorSport memakai method abstarct dari Operasi Gergigi sehingga terjadi overrding
+        //Jika tidak diovveding maka Class motorSport akan eror
         @Override
         public void tambahGergigi() {
             // TODO Auto-generated method stub
